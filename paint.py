5b4da9d77fceb667f2e424cfb405709fcfb6ee38
elnalguichi <elnalguichi@users.noreply.github.com>
2021-05-05 20:08:05 -0500

    Se modifica la función del triángulo

--- paint.py
+++ paint.py
@@ -53,9 +53,22 @@ def rectangle(start, end):
 
     end_fill()
 def triangle(start, end):
-    "Draw triangle from start to end."
-    pass  # TODO
+    "Draw rectangle from start to end."
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
 
+    for count in range(1):
+        forward(end.x - start.x)
+        left(120)
+        forward(end.x- start.x)
+
+
+
+    end_fill()
+
+               
 def tap(x, y):
     "Store starting point or draw shape."
     start = state['start']
